01c8051ba6e1e3b02a0abb5e97fefd96a0f9e38a
Aaron Triantafyllidis and Rick Reilly <pair+atriantafyllidis+rreilly@pivotallabs.com>
2014-09-05 15:36:51 -0400

    consume custom build rubies

--- lib/cloud_foundry/language_pack/extensions.rb
+++ lib/cloud_foundry/language_pack/extensions.rb
@@ -5,12 +5,16 @@ DEPENDENCIES_TRANSLATION_DELIMITER = '_'
 require 'cloud_foundry/language_pack/fetcher'
 require 'cloud_foundry/language_pack/ruby'
 require 'cloud_foundry/language_pack/helpers/plugins_installer'
-require 'cloud_foundry/language_pack/helpers/readline_symlink'
 
 module LanguagePack
   module Extensions
     def self.translate(host_url, original_filename)
-      prefix = host_url.to_s.gsub(DEPENDENCIES_TRANSLATION_REGEX, DEPENDENCIES_TRANSLATION_DELIMITER)
+      if original_filename.include?("ruby-2.1")
+        prefix = "http://build-ruby.10.244.0.34.xip.io".to_s.gsub(DEPENDENCIES_TRANSLATION_REGEX, DEPENDENCIES_TRANSLATION_DELIMITER)
+      else
+        prefix = host_url.to_s.gsub(DEPENDENCIES_TRANSLATION_REGEX, DEPENDENCIES_TRANSLATION_DELIMITER)
+      end
+
       "#{prefix}#{delimiter_for(prefix)}#{original_filename}"
     end
 

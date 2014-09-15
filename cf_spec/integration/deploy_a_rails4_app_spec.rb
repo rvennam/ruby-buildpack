6adfd85bbf6136e1b760ab14912a8c5b686f507b
Colin Humphreys and Rasheed Abdul-Aziz <pair+hatofmonkeys+squeedee@pivotallabs.com>
2014-09-15 15:18:46 -0400

    Move Rails4 test to Machete TNG.
    Removes vendored gems from the rails4 fixture app.

--- cf_spec/integration/deploy_a_rails4_app_spec.rb
+++ cf_spec/integration/deploy_a_rails4_app_spec.rb
@@ -1,54 +1,58 @@
 $: << 'cf_spec'
 require 'cf_spec_helper'
+module Machete
+  describe 'Rails 4 App' do
+    let(:browser) { Browser.new }
+    let(:firewall_log) { FirewallLog.new }
+    let(:focker) { Focker.new }
 
-# describe 'Rails 4 App' do
-#   subject(:app) { Machete.deploy_app(app_name, with_pg: true) }
-#   let(:browser) { Machete::Browser.new(app) }
-#
-#   context 'in an offline environment', if: Machete::BuildpackMode.offline? do
-#     let(:app_name) { 'rails4_web_app' }
-#
-#     specify do
-#       expect(app).to be_running
-#
-#       browser.visit_path('/')
-#       expect(browser).to have_body('The Kessel Run')
-#
-#       expect(app.host).not_to have_internet_traffic
-#     end
-#
-#   end
-#
-#   context 'in an online environment', if: Machete::BuildpackMode.online? do
-#     context 'app has dependencies' do
-#       let(:app_name) { 'rails4_web_app' }
-#
-#       specify do
-#         expect(app).to be_running
-#
-#         browser.visit_path('/')
-#         expect(browser).to have_body('The Kessel Run')
-#       end
-#     end
-#
-#     context 'app has no dependencies' do
-#       let(:app_name) { 'rails4_web_app_without_vendored_dependencies' }
-#
-#       specify do
-#         expect(Dir.exists?("cf_spec/fixtures/#{app_name}/vendor")).to eql(false)
-#         expect(app).to be_running
-#
-#         browser.visit_path('/')
-#         expect(browser).to have_body('The Kessel Run')
-#       end
-#     end
-#   end
-# end
-#
-#
-#
-#
-#
-#
-#
-#
+    before(:all) do
+      Focker.stop_staging
+      sleep 1
+    end
+
+    context 'in an offline environment' do
+      before do
+        Machete.use_offline_environment
+      end
+
+      specify do
+        focker.vendor_dependencies('./cf_spec/fixtures/rails4_web_app')
+        focker.deploy_app('./cf_spec/fixtures/rails4_web_app')
+        expect(focker).to have_successfully_deployed_app
+
+        browser.visit('/')
+        expect(browser).to have_body('The Kessel Run')
+        expect(firewall_log).to_not have_egress_attempts
+      end
+    end
+
+    context 'in an online environment' do
+      before do
+        Machete.use_online_environment
+      end
+
+      context 'app has dependencies' do
+        specify do
+          focker.vendor_dependencies('./cf_spec/fixtures/rails4_web_app')
+          focker.deploy_app('./cf_spec/fixtures/rails4_web_app')
+          expect(focker).to have_successfully_deployed_app
+
+          browser.visit('/')
+          expect(browser).to have_body('The Kessel Run')
+        end
+      end
+
+      context 'app has no dependencies' do
+        specify do
+          focker.remove_vendored_dependencies('./cf_spec/fixtures/rails4_web_app')
+          focker.deploy_app('./cf_spec/fixtures/rails4_web_app')
+          expect(focker).to have_successfully_deployed_app
+
+          browser.visit('/')
+          expect(browser).to have_body('The Kessel Run')
+        end
+      end
+    end
+  end
+end

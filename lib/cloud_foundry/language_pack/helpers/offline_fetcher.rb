ad24ac32c80ba22a60c5faaeb5cb243408b9a8f1
Dave Goddard, David Morhovich and Rick Reilly <pair+dgoddard+dmorhovich+rreilly@pivotal.io>
2014-10-16 17:30:51 -0400

    WIP

--- lib/cloud_foundry/language_pack/helpers/offline_fetcher.rb
+++ lib/cloud_foundry/language_pack/helpers/offline_fetcher.rb
@@ -5,6 +5,10 @@ class OfflineFetcher
     if DependencyExistenceChecker.exists? dependency_filename
       run_callback.call("cp #{File.join(DEPENDENCIES_PATH, dependency_filename)} #{original_filename}")
     else
+      puts("**************** fetch: dependency_filename: #{dependency_filename}")
+      puts("**************** fetch: host_url: #{host_url}")
+      puts("**************** fetch: files_to_extract: #{files_to_extract}")
+
       error_callback.call(error_message(original_filename))
     end
   end
@@ -15,7 +19,11 @@ class OfflineFetcher
     if DependencyExistenceChecker.exists? dependency_filename
       run_callback.call("tar zxf #{File.join(DEPENDENCIES_PATH, dependency_filename)} #{files_to_extract}")
     else
-      error_callback.call(error_message(original_filename))
+      puts("**************** fetch_untar: dependency_filename: #{dependency_filename}")
+      puts("**************** fetch_untar: host_url: #{host_url}")
+      puts("**************** fetch_untar: files_to_extract: #{files_to_extract}")
+
+      error_callback.call(error_message(original_filename, dependency_filename))
     end
   end
 
